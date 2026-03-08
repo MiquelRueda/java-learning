ed3c1f530e8f6837e049ef27debb680662646048
MiquelRueda <miquelderueda@gmail.com>
2026-03-08 18:57:55 +0100

    Setmana2

--- todo-api/src/main/java/com/miquelrueda/todo_api/repository/TareaRepository.java
+++ todo-api/src/main/java/com/miquelrueda/todo_api/repository/TareaRepository.java
@@ -2,7 +2,7 @@ package com.miquelrueda.todo_api.repository;
 
 import com.miquelrueda.todo_api.model.Tarea;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;a
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TareaRepository extends JpaRepository<Tarea, Long> {
