a99ae68d6204e55c3ac18a42ca2e23da718c7975
MiquelRueda <miquelderueda@gmail.com>
2026-03-08 18:57:31 +0100

    initial Spring Boot project with hello and info endpoints

--- todo-api/src/main/java/com/miquelrueda/todo_api/repository/TareaRepository.java
+++ todo-api/src/main/java/com/miquelrueda/todo_api/repository/TareaRepository.java
@@ -2,7 +2,7 @@ package com.miquelrueda.todo_api.repository;
 
 import com.miquelrueda.todo_api.model.Tarea;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;a
 
 @Repository
 public interface TareaRepository extends JpaRepository<Tarea, Long> {
